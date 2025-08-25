55ee4086949f3c39759e23d9a5370eed8359bec2
Chicha342 <86342851+Chicha342@users.noreply.github.com>
2025-08-25 14:44:15 +0300

    Initial commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Никита on 25.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitTestTests/GitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestTests.swift
+//  GitTestTests
+//
+//  Created by Никита on 25.08.2025.
+//
+
+import Testing
+@testable import GitTest
+
+struct GitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
